74fd9db8f7ea8bac76927ce1ce5ef30eb4e09f94
Manoj6425 <87978794+Manoj6425@users.noreply.github.com>
2022-07-07 09:20:42 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is a React App Demo</h1>
+      <p>Hi I am Manoj! I am a Frontend Developer </p>
+    </div>
+  );
+}
+
+export default Notes;
